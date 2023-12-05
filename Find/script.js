bb2e5e9561437bf8d63ddeb3fdb33a2a90d9a336
jiwon-choii <144146090+jiwon-choii@users.noreply.github.com>
2023-12-05 08:47:04 -0500

    updated

---
+++ Find/script.js
@@ -0,0 +1,28 @@
+$(window).scroll(function() {
+    alert('Find your Favorite!');
+ });
+ 
+ var spotlight = document.querySelector(".spotlight");
+ var isUserMotion = false;
+ 
+ function move(e) {
+   if (e.clientX || e.touches) {
+     isUserMotion = true;
+     spotlight.style.setProperty(
+       "--x",
+       (e.clientX || e.touches[0].clientX) + "px"
+     );
+     spotlight.style.setProperty(
+       "--y",
+       (e.clientY || e.touches[0].clientY) + "px"
+     );
+   }
+ }
+ 
+ if (window.PointerEvent) {
+   spotlight.addEventListener("pointermove", move);
+ } else {
+   spotlight.addEventListener("touchmove", move);
+   spotlight.addEventListener("mousemove", move);
+ }
+ 
